f549ea3037c812ed76a1d2a1bc6b9600ccc064f8
Alex <alexdesouzasilva94@gmail.com>
2024-11-02 09:06:08 -0300

    add

--- src/main/java/br/com/devsenior/ds_devops/resources/HeathCheck.java
+++ src/main/java/br/com/devsenior/ds_devops/resources/HeathCheck.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping(value = "/health")
 public class HeathCheck {
     
     @GetMapping
     public ResponseEntity<String> health() {
+        log.info("Chamando Health");
         String status = "Status Ok";
         return ResponseEntity.ok(status);
     }
